4943da8d9e827400684c4f1fd6e7d3e86a545dcf
Alessio <106932598+Shields321@users.noreply.github.com>
2025-10-26 19:34:29 -0400

    Add files via upload

--- ArdinoHalloweenCode.ino
+++ ArdinoHalloweenCode.ino
@@ -1,11 +1,11 @@
 //Full code
 #include <Arduino.h>
 
-#define LASER_RECEIVER_PIN 10
-#define LED 7
-#define relay1 3
-#define relay2 4
-#define relay3 12
+#define LASER_RECEIVER_PIN 53
+#define LED 51 // test LED when LED is on system is ready to run when its off system in running 
+#define relay1 49 // realy 1 is 120V plug relay
+#define relay2 47 // realy 2 is fog machine 
+#define relay3 45 // realy 3 is low voltage witch realy
 
 void setup() {
   Serial.begin(9600);
@@ -14,6 +14,7 @@ void setup() {
   pinMode(LED, OUTPUT);  
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
   
   digitalWrite(relay1, LOW);
   digitalWrite(relay2, LOW);
@@ -34,7 +35,7 @@ void loop() {
     delay(5*1000);  // Delay for 5 seconds
     digitalWrite(relay1, LOW);
     digitalWrite(relay3, LOW);
-    delay(120*1000);  // Delay for 2 min           
+    delay(120000);  // Delay for 2 min           
   }
   else{
     digitalWrite(LED, HIGH); 
